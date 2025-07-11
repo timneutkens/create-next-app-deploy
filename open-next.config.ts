5641e90d3a68d06cd8a0ceb49ed539369b5d3eac
Tim Neutkens <tim@timneutkens.nl>
2025-07-11 09:45:27 +0200

    Changes

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+// open-next.config.ts
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
+
+export default defineCloudflareConfig({
+  incrementalCache: staticAssetsIncrementalCache,
+});
